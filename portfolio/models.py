9a2f5060a5719baa860f8c182fa8f85df7b4a126
William <guillermoalvarez92@hotmail.com>
2021-12-10 16:28:41 +0000

    image

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
 # Create your models here.
 class Portfolio (models.Model):
     title = models.CharField(max_length=100, null=True)
     repo_link = models.URLField(max_length=500, null=True)
     live_link = models.URLField(max_length=500, null=True)
     description = models.CharField(max_length=800, null=True)
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True, upload_to='images/')
